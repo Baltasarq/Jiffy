4c6dd0b1e95e20083fb796105d8f4912cebbf5fd
baltasarq <baltasarq@gmail.com>
2023-06-26 11:27:21 +0200

    First commit skeleton of basic CLI tool

---
+++ src/AppInfo.java
@@ -0,0 +1,5 @@
+public class AppInfo {
+    public static String Name = "Jiffy";
+    public static String Version = "v0.1 20230708";
+    public static String CompleteName = Name + " " + Version;
+}
