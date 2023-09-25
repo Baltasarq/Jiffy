91f3f09b265477fa791516a770a7c6076540229f
baltasarq <baltasarq@gmail.com>
2023-09-25 15:23:15 +0200

    Bumps version

--- src/core/AppInfo.java
+++ src/core/AppInfo.java
@@ -6,6 +6,6 @@ package core;
 
 public class AppInfo {
     public static String Name = "Jiffy";
-    public static String Version = "v0.1 20230915";
+    public static String Version = "v0.2 20230929";
     public static String CompleteName = Name + " " + Version;
 }
